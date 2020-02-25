01a3d88f43d66bb90b3049d817ea04185019dba4
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-02-25 13:52:01 -0500

    fix block-spacing
    Co-authored-by: Alejandro E Rodriquez <55757912+imxande@users.noreply.github.com>
    Co-authored-by: Ariana Shackelford <26654712+ArianaShackelford@users.noreply.github.com>
    Co-authored-by: Faye Skeen <50350459+fskeen@users.noreply.github.com>
    Co-authored-by: Nathan Saygers <48068572+nathan-saygers@users.noreply.github.com>
    Co-authored-by: Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
    Co-authored-by: Tyge Davis <53713175+tygedavis@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -145,17 +145,20 @@ module.exports = {
 
         /* keywords & blocks */
 
+        'keyword-spacing': [
+            'error', { 'before': true, 'after': true },
+        ],
         'brace-style': [
             'error', 'stroustrup', { 'allowSingleLine': true },
         ],
         'curly': [
             'error', 'all',
         ],
-        'space-before-blocks': [
+        'block-spacing': [
             'error', 'always',
         ],
-        'keyword-spacing': [
-            'error', { 'before': true, 'after': true },
+        'space-before-blocks': [
+            'error', 'always',
         ],
 
         /* functions */
