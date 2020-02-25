199464feb5ebba628c64e21dd79496bff13ea139
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-02-24 19:28:31 -0500

    lint the linter config :P
    Co-authored-by: Alejandro E Rodriquez <55757912+imxande@users.noreply.github.com>
    Co-authored-by: Ariana Shackelford <26654712+ArianaShackelford@users.noreply.github.com>
    Co-authored-by: Faye Skeen <50350459+fskeen@users.noreply.github.com>
    Co-authored-by: Nathan Saygers <48068572+nathan-saygers@users.noreply.github.com>
    Co-authored-by: Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
    Co-authored-by: Tyge Davis <53713175+tygedavis@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,288 +1,288 @@
 module.exports = {
 
-  'env': {
-    'node': true,
-    'es6': true,
-  },
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly',
-  },
+    'env': {
+        'node': true,
+        'es6': true,
+    },
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly',
+    },
 
-  /**********************************************************/
+    /**********************************************************/
 
-  'plugins': [
-    'import',
-    /* 'jsx-a11y', */
-    'react',
-    'react-native',
-    /* 'react-native-a11y', */
-    '@typescript-eslint',
-  ],
-  'extends': [
-    'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    /* 'plugin:jsx-a11y/recommended', */
-    'plugin:react/recommended',
+    'plugins': [
+        'import',
+        /* 'jsx-a11y', */
+        'react',
+        'react-native',
+        /* 'react-native-a11y', */
+        '@typescript-eslint',
+    ],
+    'extends': [
+        'eslint:recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        /* 'plugin:jsx-a11y/recommended', */
+        'plugin:react/recommended',
     /* 'plugin:react-native-a11y/recommended', */
-  ],
-  'parser': 'babel-eslint',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+    ],
+    'parser': 'babel-eslint',
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true,
+        },
+        'ecmaVersion': 2018,
+        'sourceType': 'module',
     },
-    'ecmaVersion': 2018,
-    'sourceType': 'module',
-  },
-  'overrides': [
-    {
-      'files': [ '*.ts', '*.tsx' ],
-      'extends': [
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-      ],
-      'parser': '@typescript-eslint/parser',
-    },
-  ],
+    'overrides': [
+        {
+            'files': [ '*.ts', '*.tsx' ],
+            'extends': [
+                'plugin:@typescript-eslint/eslint-recommended',
+                'plugin:@typescript-eslint/recommended',
+            ],
+            'parser': '@typescript-eslint/parser',
+        },
+    ],
 
-  /**********************************************************/
+    /**********************************************************/
 
-  'rules': {
+    'rules': {
 
-    /***************************************
+        /***************************************
       style
     ***************************************/
 
-    /* general spacing */
+        /* general spacing */
 
-    'indent': [
-      'error', 4,
-    ],
-    'linebreak-style': [
-      'error', 'unix',
-    ],
-    'eol-last': [
-      'error', 'always',
-    ],
-    'no-irregular-whitespace': [
-      'error',
-    ],
-    'no-multi-spaces': [
-      'error',
-    ],
-    'no-trailing-spaces': [
-      'error',
-    ],
-    'no-multiple-empty-lines': [
-      'error', { 'max': 2 },
-    ],
+        'indent': [
+            'error', 4,
+        ],
+        'linebreak-style': [
+            'error', 'unix',
+        ],
+        'eol-last': [
+            'error', 'always',
+        ],
+        'no-irregular-whitespace': [
+            'error',
+        ],
+        'no-multi-spaces': [
+            'error',
+        ],
+        'no-trailing-spaces': [
+            'error',
+        ],
+        'no-multiple-empty-lines': [
+            'error', { 'max': 2 },
+        ],
 
-    /* comments */
+        /* comments */
 
-    'spaced-comment': [
-      'error', 'always', { 'exceptions': [ '/', '*', '+', '-', '=' ] },
-    ],
+        'spaced-comment': [
+            'error', 'always', { 'exceptions': [ '/', '*', '+', '-', '=' ] },
+        ],
 
-    /* dots */
+        /* dots */
 
-    'no-whitespace-before-property': [
-      'error',
-    ],
-    'dot-location': [
-      'error', 'property',
-    ],
+        'no-whitespace-before-property': [
+            'error',
+        ],
+        'dot-location': [
+            'error', 'property',
+        ],
 
-    /* commas */
+        /* commas */
 
-    'comma-style': [
-      'error', 'last',
-    ],
-    'comma-dangle': [
-      'error',
-      {
-        'arrays': 'always-multiline',
-        'exports': 'always-multiline',
-        'functions': 'always-multiline',
-        'imports': 'always-multiline',
-        'objects': 'always-multiline',
-      },
-    ],
-    'comma-spacing': [
-      'error', { 'before': false, 'after': true },
-    ],
+        'comma-style': [
+            'error', 'last',
+        ],
+        'comma-dangle': [
+            'error',
+            {
+                'arrays': 'always-multiline',
+                'exports': 'always-multiline',
+                'functions': 'always-multiline',
+                'imports': 'always-multiline',
+                'objects': 'always-multiline',
+            },
+        ],
+        'comma-spacing': [
+            'error', { 'before': false, 'after': true },
+        ],
 
-    /* colons */
+        /* colons */
 
-    'key-spacing': [
-      'error', { 'beforeColon': false, 'afterColon': true },
-    ],
-    'switch-colon-spacing': [
-      'error', { 'before': false, 'after': true },
-    ],
+        'key-spacing': [
+            'error', { 'beforeColon': false, 'afterColon': true },
+        ],
+        'switch-colon-spacing': [
+            'error', { 'before': false, 'after': true },
+        ],
 
-    /* semicolons */
+        /* semicolons */
 
-    'semi': [
-      'error', 'always', { 'omitLastInOneLineBlock' : true },
-    ],
-    'semi-style': [
-      'error', 'last',
-    ],
-    'semi-spacing': [
-      'error', { 'before': false, 'after': true },
-    ],
+        'semi': [
+            'error', 'always', { 'omitLastInOneLineBlock': true },
+        ],
+        'semi-style': [
+            'error', 'last',
+        ],
+        'semi-spacing': [
+            'error', { 'before': false, 'after': true },
+        ],
 
-    /* quotes */
+        /* quotes */
 
-    'quotes': [
-      'error', 'single',
-    ],
-    'quote-props': [
-      'error', 'consistent',
-    ],
+        'quotes': [
+            'error', 'single',
+        ],
+        'quote-props': [
+            'error', 'consistent',
+        ],
 
-    /* parens */
+        /* parens */
 
-    'space-in-parens': [
-      'error', 'never',
-    ],
+        'space-in-parens': [
+            'error', 'never',
+        ],
 
-    /* keywords & blocks */
+        /* keywords & blocks */
 
-    'brace-style': [
-      'error', 'stroustrup', { 'allowSingleLine': true },
-    ],
-    'curly': [
-      'error', 'all',
-    ],
-    'space-before-blocks': [
-      'error', 'always',
-    ],
-    'keyword-spacing': [
-      'error', { 'before': true, 'after': true },
-    ],
+        'brace-style': [
+            'error', 'stroustrup', { 'allowSingleLine': true },
+        ],
+        'curly': [
+            'error', 'all',
+        ],
+        'space-before-blocks': [
+            'error', 'always',
+        ],
+        'keyword-spacing': [
+            'error', { 'before': true, 'after': true },
+        ],
 
-    /* functions */
+        /* functions */
 
-    'arrow-parens': [
-      'error', 'always',
-    ],
-    'arrow-spacing': [
-      'error', { 'before': true, 'after': true },
-    ],
-    'space-before-function-paren': [
-      'error',
-      {
-        'anonymous': 'always',
-        'named': 'never',
-        'asyncArrow': 'always',
-      },
-    ],
-    'function-paren-newline': [
-      'error', 'consistent',
-    ],
-    'function-call-argument-newline': [
-      'error', 'consistent',
-    ],
+        'arrow-parens': [
+            'error', 'always',
+        ],
+        'arrow-spacing': [
+            'error', { 'before': true, 'after': true },
+        ],
+        'space-before-function-paren': [
+            'error',
+            {
+                'anonymous': 'always',
+                'named': 'never',
+                'asyncArrow': 'always',
+            },
+        ],
+        'function-paren-newline': [
+            'error', 'consistent',
+        ],
+        'function-call-argument-newline': [
+            'error', 'consistent',
+        ],
 
-    /* arrays */
+        /* arrays */
 
-    'array-bracket-newline': [
-      'error', 'consistent',
-    ],
-    'array-bracket-spacing': [
-      'error', 'always',
-    ],
-    'array-element-newline': [
-      'error', 'consistent',
-    ],
+        'array-bracket-newline': [
+            'error', 'consistent',
+        ],
+        'array-bracket-spacing': [
+            'error', 'always',
+        ],
+        'array-element-newline': [
+            'error', 'consistent',
+        ],
 
-    /* objects */
+        /* objects */
 
-    'object-curly-newline': [
-      'error', { 'consistent': true },
-    ],
-    'object-curly-spacing': [
-      'error', 'always',
-    ],
-    'object-property-newline': [
-      'error', { 'allowAllPropertiesOnSameLine': true },
-    ],
+        'object-curly-newline': [
+            'error', { 'consistent': true },
+        ],
+        'object-curly-spacing': [
+            'error', 'always',
+        ],
+        'object-property-newline': [
+            'error', { 'allowAllPropertiesOnSameLine': true },
+        ],
 
-    /* operators */
+        /* operators */
 
-    'eqeqeq': [
-      'warn', 'always',
-    ],
-    'no-mixed-operators': [
-      'error',
-    ],
-    'operator-linebreak': [
-      'error', 'before',
-    ],
-    'space-infix-ops': [
-      'error',
-    ],
-    'space-unary-ops': [
-      'error', { 'words': true, 'nonwords': false },
-    ],
+        'eqeqeq': [
+            'warn', 'always',
+        ],
+        'no-mixed-operators': [
+            'error',
+        ],
+        'operator-linebreak': [
+            'error', 'before',
+        ],
+        'space-infix-ops': [
+            'error',
+        ],
+        'space-unary-ops': [
+            'error', { 'words': true, 'nonwords': false },
+        ],
 
-    /* scoping */
-    
-    'no-unused-vars': [
-      'warn',
-    ],
-    'no-var': [
-      'warn',
-    ],
-    'vars-on-top': [
-      'error',
-    ],
-    'block-scoped-var': [
-      'error',
-    ],
-    'no-use-before-define': [
-      'warn',
-    ],
-    '@typescript-eslint/no-use-before-define': [
-      'warn',
-    ],
+        /* scoping */
 
-    /***************************************
+        'no-unused-vars': [
+            'warn',
+        ],
+        'no-var': [
+            'warn',
+        ],
+        'vars-on-top': [
+            'error',
+        ],
+        'block-scoped-var': [
+            'error',
+        ],
+        'no-use-before-define': [
+            'warn',
+        ],
+        '@typescript-eslint/no-use-before-define': [
+            'warn',
+        ],
+
+        /***************************************
       react
     ***************************************/
 
-    'react/display-name': [
-      'warn',
-    ],
-    'react/prop-types': [
-      'off',
-    ],
-    'react/jsx-filename-extension': [
-      'off',
-    ],
+        'react/display-name': [
+            'warn',
+        ],
+        'react/prop-types': [
+            'off',
+        ],
+        'react/jsx-filename-extension': [
+            'off',
+        ],
 
-    /***************************************
+        /***************************************
       react-native
     ***************************************/
 
-    'react-native/no-unused-styles': [
-      'warn',
-    ],
-    'react-native/split-platform-components': [
-      'warn',
-    ],
-    'react-native/no-inline-styles': [
-      'warn',
-    ],
-    'react-native/no-color-literals': [
-      'warn',
-    ],
-    'react-native/no-raw-text': [
-      'warn',
-    ],
+        'react-native/no-unused-styles': [
+            'warn',
+        ],
+        'react-native/split-platform-components': [
+            'warn',
+        ],
+        'react-native/no-inline-styles': [
+            'warn',
+        ],
+        'react-native/no-color-literals': [
+            'warn',
+        ],
+        'react-native/no-raw-text': [
+            'warn',
+        ],
 
-  },
+    },
 
 };
