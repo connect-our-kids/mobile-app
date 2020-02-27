b3ad957d3e8e95da8be50dd7854a714574beccfb
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-02-27 13:19:36 -0500

    lint
    Co-authored-by: Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>

--- src/components/ConnectionsViewTabs/AddDocPicker.js
+++ src/components/ConnectionsViewTabs/AddDocPicker.js
@@ -1,82 +1,82 @@
 import React, { Component } from 'react';
 import * as DocumentPicker from 'expo-document-picker';
-import {
-StyleSheet,
-Text,
-TouchableOpacity,
-View } from 'react-native';
+import { StyleSheet,
+    Text,
+    TouchableOpacity,
+    View } from 'react-native';
 
 
 export default class App extends Component {
 
-constructor(props) {
-super(props);
-this.state = {
-    singleFileOBJ: '',
-};
-}
+    constructor(props) {
+        super(props);
+        this.state = {
+            singleFileOBJ: '',
+        };
+    }
 
-async SingleFilePicker() {
-try {
-    const res = await DocumentPicker.getDocumentAsync({
-    type: "*/* ",
-    copyToCacheDirectory: false,
-    multiple: false
-    });
+    async SingleFilePicker() {
+        try {
+            const res = await DocumentPicker.getDocumentAsync({
+                type: '*/* ',
+                copyToCacheDirectory: false,
+                multiple: false,
+            });
 
-    this.setState({ singleFileOBJ: res });
+            this.setState({ singleFileOBJ: res });
 
-} catch (err) {
-    console.log(err)
-}
-}
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
 
-render() {
-return (
-    <View style={styles.MainContainer}>
+    render() {
+        return (
+            <View style={styles.MainContainer}>
 
-    <Text style={styles.text}>
+                <Text style={styles.text}>
         File Name: {this.state.singleFileOBJ.name ? this.state.singleFileOBJ.name : ''}
-    </Text>
+                </Text>
 
-    <TouchableOpacity
-        activeOpacity={0.5}
-        style={styles.button}
-        onPress={this.SingleFilePicker.bind(this)}>
-        <Text style={styles.buttonText}>
+                <TouchableOpacity
+                    activeOpacity={0.5}
+                    style={styles.button}
+                    onPress={this.SingleFilePicker.bind(this)}>
+                    <Text style={styles.buttonText}>
         Click Here To Pick File
-        </Text>
-    </TouchableOpacity>
-    </View>
-);
-}
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-MainContainer: {
-flex: 1,
-backgroundColor: '#fff',
-padding: 16,
-justifyContent: 'center',
-},
+    MainContainer: {
+        flex: 1,
+        backgroundColor: '#fff',
+        padding: 16,
+        justifyContent: 'center',
+    },
 
-button: {
-width: '100%',
-backgroundColor: '#0091EA',
-borderRadius:9,
-},
+    button: {
+        width: '100%',
+        backgroundColor: '#0091EA',
+        borderRadius: 9,
+    },
 
-buttonText: {
-color: '#fff',
-fontSize: 21,
-padding: 10,
-textAlign: 'center'
-},
+    buttonText: {
+        color: '#fff',
+        fontSize: 21,
+        padding: 10,
+        textAlign: 'center',
+    },
 
-text: {
-color: '#000',
-fontSize: 16,
-padding: 10,
-textAlign: 'left'
-},
+    text: {
+        color: '#000',
+        fontSize: 16,
+        padding: 10,
+        textAlign: 'left',
+    },
 });
