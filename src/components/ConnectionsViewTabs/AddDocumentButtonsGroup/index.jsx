9dfed782cf44d0d8669708e80dbcbc777e76d2e9
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-03-02 16:00:04 -0500

    write skeleton of AddDocumentButtonsGroup
    Co-authored-by: Alejandro E Rodriquez <55757912+imxande@users.noreply.github.com>
    Co-authored-by: Ariana Shackelford <26654712+ArianaShackelford@users.noreply.github.com>
    Co-authored-by: Nathan Saygers <48068572+nathan-saygers@users.noreply.github.com>
    Co-authored-by: Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>

---
+++ src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/index.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import {
+    Text,
+    View,
+    TouchableOpacity,
+    StyleSheet,
+    Alert,
+} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import constants from '../../../helpers/constants';
+
+/**********************************************************/
+
+export default function AddDocumentButtonsGroup(props) {
+
+    const [ document, setDocuement ] = useState(null);
+
+    function pickFile() {
+        return;
+    }
+    function pickPhoto() {
+        return;
+    }
+    function takePhoto() {
+        return;
+    }
+
+    return (
+        <View>
+            <Button
+                iconName={'file'}
+                onPress={pickFile}
+            />
+            <Button
+                iconName={'photo-library'}
+                onPress={pickPhoto}
+            />
+            <Button
+                iconName={'photo-camera'}
+                onPress={takePhoto}
+            />
+        </View>
+    );
+}
+
+function Button({ onPress, iconName, ...rest }) {
+    return (
+        <TouchableOpacity
+            style={styles.imageButton}
+            onPress={onPress}
+        >
+            <MaterialIcons
+                name={iconName}
+                size={75}
+                color={constants.highlightColor}
+            />
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    viewportContainer: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'flex-start',
+        borderRadius: 4,
+    },
+    image: {
+        width: 125,
+        height: 125,
+        marginBottom: 4,
+        marginTop: 4,
+    },
+    buttonImageContainer: { width: '95%' },
+    imageButton: { width: '50%' },
+
+});
