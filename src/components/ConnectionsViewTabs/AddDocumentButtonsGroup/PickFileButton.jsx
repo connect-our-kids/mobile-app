f24d01ff7b30af47c58c8bc3ae6963b00e3ab358
nathan-saygers <nathan.saygers@gmail.com>
2020-03-03 10:00:38 -0800

    Add pickFile function
    Co-authored-by: Alejandro E Rodriquez <55757912+imxande@users.noreply.github.com>
    Co-authored-by: Ariana Shackelford <26654712+ArianaShackelford@users.noreply.github.com>
    Co-authored-by: Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
    Co-authored-by: Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>

--- src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/PickFileButton.jsx
+++ src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/PickFileButton.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import Button from './Button.jsx';
 import PickFileIcon from './PickFileIcon.jsx';
+import * as DocumentPicker from 'expo-document-picker';
 
 /**********************************************************/
 
 export default function PickFileButton({ setDocument }) {
 
     function pickFile() {
-        return;
+        DocumentPicker
+            .getDocumentAsync({
+                /* MIME type */
+                type: '*/*',
+                /* whether to cache file for other expo APIs to use */
+                copyToCacheDirectory: false,
+                /* whether to let user select multiple files */
+                multiple: false,
+            })
+            .then ((re) => {
+                setDocument(re);
+            })
+            .catch ((error) => {
+                console.error('--- There was a problem selecting a file. ---');
+                console.log(error);
+            });
     }
 
     return (
