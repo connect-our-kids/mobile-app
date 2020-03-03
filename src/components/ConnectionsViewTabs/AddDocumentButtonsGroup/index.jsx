0fbb32a8a52a2d4965dfbbb29573c9180f3b5659
Ariana Shackelford <ariana.shackelford@gmail.com>
2020-03-03 17:09:29 -0500

    Styles buttons in index.jsx of AddDocumentButtonsGroup
    Co-authored-by: Alejandro E Rodriquez <55757912+imxande@users.noreply.github.com>
    Co-authored-by: Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
    Co-authored-by: Nathan Saygers <48068572+nathan-saygers@users.noreply.github.com>
    Co-authored-by: Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>

--- src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/index.jsx
+++ src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/index.jsx
@@ -3,6 +3,7 @@ import { View } from 'react-native';
 import PickFileButton from './PickFileButton.jsx';
 import PickPhotoButton from './PickPhotoButton.jsx';
 import TakePhotoButton from './TakePhotoButton.jsx';
+import styles from './styles.js';
 
 /**********************************************************/
 
@@ -11,7 +12,7 @@ export default function AddDocumentButtonsGroup(props) {
     const [ document, setDocument ] = useState(null);
 
     return (
-        <View>
+        <View style={styles.buttonsGroup}>
             <PickFileButton setDocument={setDocument} />
             <PickPhotoButton setDocument={setDocument} />
             <TakePhotoButton setDocument={setDocument} />
