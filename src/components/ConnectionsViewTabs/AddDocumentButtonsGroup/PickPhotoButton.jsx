fe7ea02be967eee407ff2454f4ad84322c3e7fa5
Ariana Shackelford <ariana.shackelford@gmail.com>
2020-03-03 14:03:20 -0500

    Added getPermissions to PickPhotoButton.jsx
    Co-authored-by: Alejandro E Rodriquez <55757912+imxande@users.noreply.github.com>
    Co-authored-by: Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
    Co-authored-by: Nathan Saygers <48068572+nathan-saygers@users.noreply.github.com>
    Co-authored-by: Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>

--- src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/PickPhotoButton.jsx
+++ src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/PickPhotoButton.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Button from './Button.jsx';
+import { Alert } from 'react-native';
 import PickPhotoIcon from './PickPhotoIcon.jsx';
 import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+import Constants from 'expo-constants';
 
 /**********************************************************/
 
 export default function PickPhotoButton({ setDocument }) {
+
+    async function getPermissions() {
+        let hasPermissions = false;
+        if (Constants.platform.ios || Constants.platform.android) {
+            const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL || Permissions.CAMERA);
+            hasPermissions = status === 'granted';
+        }
+        return hasPermissions;
+    }
 
     async function pickPhoto() {
         const result = await ImagePicker.launchImageLibraryAsync({
