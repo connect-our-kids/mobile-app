9562a22500604d6c3b56040efe719d46749c66b1
imxande <alerodriguezlarosa@gmail.com>
2020-03-03 13:38:33 -0500

    Have getPermissions return status
    Co-authored-by: Ariana Shackelford <ariana.shackelford@gmail.com>
    Co-authored-by: Nathan Saygers <48068572+nathan-saygers@users.noreply.github.com>
    Co-authored-by: Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
    Co-authored-by: Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>

--- src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/TakePhotoButton.jsx
+++ src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/TakePhotoButton.jsx
@@ -6,13 +6,16 @@ import TakePhotoIcon from './TakePhotoIcon.jsx';
 
 export default function TakePhotoButton({ setDocument }) {
 
+
    async function getPermissions(){
-    if (Constants.platform.ios || Constants.platform.android) {
-        const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL || Permissions.CAMERA);
-        if (status !== 'granted') {
-            Alert.alert('Sorry, we need camera roll permissions to make this work!');
+        if (Constants.platform.ios || Constants.platform.android) {
+            const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL || Permissions.CAMERA);
+            const hasPermission = status === 'granted';
+            if (!hasPermission) {
+                Alert.alert('Sorry, we need camera roll permissions to make this work!');
+            }
         }
-     }
+        return hasPermission;
     }
 
     async function takePhoto() {
