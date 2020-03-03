a8ac560d5e9dec7101b5242c7d2f96c61fc9e163
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-03-03 11:00:47 -0500

    reflect changes in index.js
    Co-authored-by: Alejandro E Rodriquez <55757912+imxande@users.noreply.github.com>
    Co-authored-by: Ariana Shackelford <26654712+ArianaShackelford@users.noreply.github.com>
    Co-authored-by: Nathan Saygers <48068572+nathan-saygers@users.noreply.github.com>
    Co-authored-by: Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>

--- src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/index.jsx
+++ src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/index.jsx
@@ -1,13 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import {
-    Text,
-    View,
-    TouchableOpacity,
-    StyleSheet,
-    Alert,
-} from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
-import constants from '../../../helpers/constants';
+import { View } from 'react-native';
+import PickFileButton from './PickFileButton.jsx';
+import PickPhotoButton from './PickPhotoButton.jsx';
+import TakePhotoButton from './TakePhotoButton.jsx';
 
 /**********************************************************/
 
@@ -15,63 +10,11 @@ export default function AddDocumentButtonsGroup(props) {
 
     const [ document, setDocument ] = useState(null);
 
-    function pickFile() {
-        return;
-    }
-    function pickPhoto() {
-        return;
-    }
-    function takePhoto() {
-        return;
-    }
-
     return (
         <View>
-            <Button
-                iconName={'insert-drive-file'}
-                onPress={pickFile}
-            />
-            <Button
-                iconName={'photo-library'}
-                onPress={pickPhoto}
-            />
-            <Button
-                iconName={'photo-camera'}
-                onPress={takePhoto}
-            />
+            <PickFileButton/>
+            <PickPhotoButton/>
+            <TakePhotoButton/>
         </View>
     );
 }
-
-function Button({ onPress, iconName, ...rest }) {
-    return (
-        <TouchableOpacity
-            style={styles.imageButton}
-            onPress={onPress}
-        >
-            <MaterialIcons
-                name={iconName}
-                size={75}
-                color={constants.highlightColor}
-            />
-        </TouchableOpacity>
-    );
-}
-
-const styles = StyleSheet.create({
-    viewportContainer: {
-        width: '100%',
-        height: '100%',
-        justifyContent: 'flex-start',
-        borderRadius: 4,
-    },
-    image: {
-        width: 125,
-        height: 125,
-        marginBottom: 4,
-        marginTop: 4,
-    },
-    buttonImageContainer: { width: '95%' },
-    imageButton: { width: '50%' },
-
-});
