01691836f86429c0bcc11e4ffbc89975a61585c2
nathan-saygers <nathan.saygers@gmail.com>
2020-03-03 09:59:07 -0800

    Update button style
    Co-authored-by: Alejandro E Rodriquez <55757912+imxande@users.noreply.github.com>
    Co-authored-by: Ariana Shackelford <26654712+ArianaShackelford@users.noreply.github.com>
    Co-authored-by: Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
    Co-authored-by: Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>

--- src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/styles.js
+++ src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/styles.js
@@ -1,5 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    button: {},
+    button: {
+        backgroundColor: 'black',
+        color: 'white',
+        borderRadius: 25,
+        width: '30%',
+    },
 });
