31885de37f7bcbb0b26797d2b0f960d40388b79a
nathan-saygers <nathan.saygers@gmail.com>
2020-03-04 10:28:41 -0800

    Remove document state
    Co-authored-by: Alejandro E Rodriquez <55757912+imxande@users.noreply.github.com>
    Co-authored-by: Ariana Shackelford <26654712+ArianaShackelford@users.noreply.github.com>
    Co-authored-by: Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
    Co-authored-by: Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>

--- src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/index.jsx
+++ src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import PickFileButton from './PickFileButton.jsx';
 import PickPhotoButton from './PickPhotoButton.jsx';
@@ -8,14 +8,11 @@ import styles from './styles.js';
 /**********************************************************/
 
 export default function AddDocumentButtonsGroup({ afterAccept }) {
-
-    const [ document, setDocument ] = useState(null);
-
     return (
         <View style={styles.buttonsGroup}>
-            <PickFileButton setDocument={setDocument} afterAccept={afterAccept}/>
-            <PickPhotoButton setDocument={setDocument} afterAccept={afterAccept}/>
-            <TakePhotoButton setDocument={setDocument} afterAccept={afterAccept}/>
+            <PickFileButton afterAccept={afterAccept}/>
+            <PickPhotoButton afterAccept={afterAccept}/>
+            <TakePhotoButton afterAccept={afterAccept}/>
         </View>
     );
 }
