503ce1c89c6a50193854d3422aecb191ac421150
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-03-12 11:07:37 -0400

    add `console.debug`s to convert[File,Photo]ToMedia
    Co-authored-by: Alejandro E Rodriquez <55757912+imxande@users.noreply.github.com>
    Co-authored-by: Ariana Shackelford <26654712+ArianaShackelford@users.noreply.github.com>
    Co-authored-by: Nathan Saygers <48068572+nathan-saygers@users.noreply.github.com>
    Co-authored-by: Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>

--- src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/convertFileToMedia.js
+++ src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/convertFileToMedia.js
@@ -13,11 +13,23 @@ export default function convertFileToMedia(file) {
     }
     */
 
-    return {
+    console.debug ('--- convertFileToMedia(file) => (media) ---');
+    console.debug ('file:');
+    console.debug (file);
+    console.debug ('--------');
+
+    const media = {
         type: 'file',
         uri: file.uri,
         name: parseBaseNameFromUri(file.name), // ... as a precaution
         size: file.size,
     };
+
+    console.debug ('--- convertFileToMedia(file) => (media) ---');
+    console.debug ('media:');
+    console.debug (media);
+    console.debug ('--------');
+
+    return media;
 
 }
--- src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/convertPhotoToMedia.js
+++ src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/convertPhotoToMedia.js
@@ -16,12 +16,24 @@ export default function convertPhotoToMedia(photo) {
     }
     */
 
-    return {
+    console.debug ('--- convertPhotoToMedia(photo) => (media) ---');
+    console.debug ('photo:');
+    console.debug (photo);
+    console.debug ('--------');
+
+    const media = {
         type: photo.type,
         uri: photo.uri,
         name: parseBaseNameFromUri(photo.uri),
         width: photo.width,
         height: photo.height,
     };
+
+    console.debug ('--- convertPhotoToMedia(photo) => (media) ---');
+    console.debug ('media:');
+    console.debug (media);
+    console.debug ('--------');
+
+    return media;
 
 }
