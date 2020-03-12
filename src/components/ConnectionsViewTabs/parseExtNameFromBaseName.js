09483e546f1038eadeaf6ee69206eed6ac8eec97
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-03-12 13:12:47 -0400

    add parseExtNameFromBaseName it does what it says
    Co-authored-by: Alejandro E Rodriquez <55757912+imxande@users.noreply.github.com>
    Co-authored-by: Ariana Shackelford <26654712+ArianaShackelford@users.noreply.github.com>
    Co-authored-by: Nathan Saygers <48068572+nathan-saygers@users.noreply.github.com>
    Co-authored-by: Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>

---
+++ src/components/ConnectionsViewTabs/parseExtNameFromBaseName.js
@@ -0,0 +1,21 @@
+export default function parseExtNameFromBaseName(path) {
+
+    /*
+    clear everything but the LAST extension:
+    -   clear characters before and including the LAST '.'
+    -   ...unless the LAST '.' is the FIRST character (accomodates "dot" names without extensions)
+    -   clear all characters when there is no '.' (accomdates names without extensions)
+    */
+    return path.replace(/^(.+(?=.*?[.])[.](?=[^.]+)|[^.]+)/, '');
+
+    /*
+    ### EXAMPLES ###
+    -   'base-name.ext'  -> 'ext'
+    -   'base.name.ext'  -> 'ext'
+    -   'base-name'      -> ''
+    -   '.base-name.ext' -> 'ext'
+    -   '.base.name.ext' -> 'ext'
+    -   '.base-name'     -> ''
+    */
+
+}
