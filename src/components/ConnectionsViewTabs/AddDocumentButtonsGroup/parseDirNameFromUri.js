0b99e6bc5c531b8c82cafdcc4cc4f6193dd53477
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-03-11 23:17:05 -0400

    add uri parsing funs
    Co-authored-by: Alejandro E Rodriquez <55757912+imxande@users.noreply.github.com>
    Co-authored-by: Ariana Shackelford <26654712+ArianaShackelford@users.noreply.github.com>
    Co-authored-by: Nathan Saygers <48068572+nathan-saygers@users.noreply.github.com>
    Co-authored-by: Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>

---
+++ src/components/ConnectionsViewTabs/AddDocumentButtonsGroup/parseDirNameFromUri.js
@@ -0,0 +1,13 @@
+import URI from 'uri-js';
+import parseDirNameFromPath from './parseDirNameFromPath';
+
+/**********************************************************/
+
+export default function parseDirNameFromUri(uri) {
+
+    // take 'path' from URI, removing 'scheme', 'host', 'query', 'fragment'
+    const path = URI.parse(uri).path;
+
+    return parseDirNameFromPath(path);
+
+}
