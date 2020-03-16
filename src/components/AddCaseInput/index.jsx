55d88ce59a40ded9d22d473a7291bd9808d392f9
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-03-16 16:51:50 -0400

    AddCaseInput: restructure as dir + cleanup
    Co-authored-by: Alejandro E Rodriquez <55757912+imxande@users.noreply.github.com>
    Co-authored-by: Ariana Shackelford <26654712+ArianaShackelford@users.noreply.github.com>
    Co-authored-by: Nathan Saygers <48068572+nathan-saygers@users.noreply.github.com>
    Co-authored-by: Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>

---
+++ src/components/AddCaseInput/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import {
+    View,
+    StyleSheet,
+} from 'react-native';
+
+import {
+    FormLabel,
+    FormInput,
+    FormValidationMessage,
+} from 'react-native-elements';
+
+/**********************************************************/
+
+export default function AddCaseInput(props) {
+
+    const { label, error, ...rest } = props;
+
+    const styles = StyleSheet.create({
+        root: {
+            width: '90%',
+            alignSelf: 'center',
+        },
+    });
+
+    function handleChange(value) {
+        props.onChange(props.name, value);
+    }
+
+    function handleTouch(value) {
+        props.onTouch(props.name, value);
+    }
+
+    return (
+        <View style={styles.root}>
+            <FormLabel>{label}</FormLabel>
+            <FormInput
+                onChangeText={handleChange}
+                onBlur={handleTouch}
+                placeholder={label}
+                {...rest}
+            />
+            {error && <FormValidationMessage>{error}</FormValidationMessage>}
+        </View>
+    );
+}
