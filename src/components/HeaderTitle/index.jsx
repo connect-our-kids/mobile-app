c1ea5cc0309d018a544f2f4a30b97f3536d35825
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-03-18 14:10:36 -0400

    HeaderTitle: cleanup -- not in auth/

--- src/components/HeaderTitle/index.jsx
+++ src/components/HeaderTitle/index.jsx
@@ -15,7 +15,16 @@ import { connect } from 'react-redux';
 
 /**********************************************************/
 
+function mapStateToProps(state) {
+
+    return {
+        email: state.auth.user ? state.auth.user.email : null,
+    };
+
+}
+
 function HeaderTitle(props) {
+
     // title should be the string of the components name
     return (
         <View style={{
@@ -24,40 +33,42 @@ function HeaderTitle(props) {
             width: '100%',
         }}>
             {/* on android the text renders left aligned and therefore we put the logo next to it, ios renders centered */}
-            {(Platform.OS === 'android') ? (
-                <TouchableWithoutFeedback
-                    onPress={() => {
-                        props.navigation.navigate('FamilyConnections');
-                        sendEvent(props.email, 'click', 'logo');
-                    }}
-                >
-                    <Image
-                        source={logoImg}
-                        style={{
-                            width: 225,
-                            height: 90,
+            {(Platform.OS === 'android')
+                ? (
+                    <TouchableWithoutFeedback
+                        onPress={() => {
+                            props.navigation.navigate('FamilyConnections');
+                            sendEvent(props.email, 'click', 'logo');
                         }}
-                        resizeMode="contain"
-                    />
-                </TouchableWithoutFeedback>
-            ) : null}
+                    >
+                        <Image
+                            source={logoImg}
+                            style={{
+                                width: 225,
+                                height: 90,
+                            }}
+                            resizeMode="contain"
+                        />
+                    </TouchableWithoutFeedback>
+                )
+                : null
+            }
             {/* <Text style={styles.text}>{title}</Text> */}
 
         </View>
     );
+
 }
 
 // const styles = StyleSheet.create({
+//
 //   text: {
 //     color: 'white',
 //     fontSize: 25,
 //     fontFamily: constants.headerFont
 //   }
+//
 // });
-
-const mapStateToProps = (state) => {
-    return { email: state.auth.user ? state.auth.user.email : null };
-};
 
 export default connect(
     mapStateToProps,
