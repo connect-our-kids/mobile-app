2bb1fbaf200ec5e8614378f3471d674a76f7a7d5
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-03-18 14:12:08 -0400

    ConnectionsLogin: cleanup

--- src/components/auth/ConnectionsLogin/index.tsx
+++ src/components/auth/ConnectionsLogin/index.tsx
@@ -1,73 +1,101 @@
-// no typescript needed
-
 import React from 'react';
-import { View, Text, StyleSheet, Linking } from 'react-native';
+
+import {
+    View,
+    Text,
+    StyleSheet,
+    Linking,
+} from 'react-native';
+
 import { Button } from 'native-base';
-import constants from '../../../helpers/constants';
-import { sendEvent } from '../../../helpers/createEvent';
-import NavigationButton from '../../../UI/NavigationButton';
+
 import MainText from '../../../UI/MainText';
 import ScreenContainer from '../../../UI/ScreenContainer';
 import authHelpers from '../../../helpers/authHelpers';
 
-const ConnectionsLogin = (props) => {
+import { sendEvent } from '../../../helpers/createEvent';
 
-    const learnMorePressed = () => {
+/**********************************************************/
+
+export default function ConnectionsLogin(props): JSX.Element {
+
+    function learnMorePressed(): void {
+
         Linking.openURL('https://www.connectourkids.org/tools/family-connections');
-    };
+
+    }
 
     return (
         <ScreenContainer>
             <MainText>
-                {'Family Connections helps social workers, family recruiters, and CASA volunteeers identify and engage extended family members of children in foster care.'}
+                Family Connections helps social workers, family recruiters, and CASA volunteeers identify and engage extended family members of children in foster care.
             </MainText>
-            <Text style={styles.linkText} onPress={learnMorePressed}>Learn More About Family Connections</Text>
+            <Text
+                style={styles.linkText}
+                onPress={learnMorePressed}
+            >
+                Learn More About Family Connections
+            </Text>
             <View style={styles.linkContainer}>
                 <View style={styles.logInBtns}>
                     <Button
                         style={styles.buttonStyle}
                         block
-                        onPress={() =>
+                        onPress={(): void =>
                             authHelpers.handleLogin(
                                 authHelpers._loginWithAuth0,
                                 props.setUserCreds,
-                            )}>
-                        <Text style={styles.btnText}>Login</Text>
+                            )}
+                    >
+                        <Text style={styles.btnText}>
+                            Login
+                        </Text>
                     </Button>
                     <Button
                         style={styles.buttonStyle}
                         block
-                        onPress={() => {
-                            props.setModalVisible(true); { /* {props.setModalVisible} */ }
+                        onPress={(): void => {
+                            props.setModalVisible(true);
                             sendEvent(null, 'click', 'sign-up');
                         }}
-                    ><Text style={styles.btnText}>Sign Up</Text>
+                    >
+                        <Text style={styles.btnText}>
+                            Sign Up
+                        </Text>
                     </Button>
                 </View>
             </View>
         </ScreenContainer>
     );
-};
+
+}
+
+/**********************************************************/
 
 const styles = StyleSheet.create({
+
     logInBtns: {
         flexDirection: 'row',
         flex: 1,
         justifyContent: 'space-evenly',
     },
-    logOutText: {
-        color: '#fff',
-    },
+
+    // logOutText: {
+    //     color: '#fff',
+    // },
+
     linkText: {
         color: '#0279AC',
         marginTop: 20,
         textAlign: 'center',
     },
+
     linkContainer: {
         justifyContent: 'space-between',
         flex: 1,
         marginTop: 40,
     },
+
     buttonStyle: {
         flex: 1,
         marginHorizontal: 5,
@@ -76,17 +104,16 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         backgroundColor: '#0279AC',
     },
+
     btnText: {
         color: '#fff',
     },
-    button: {
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        flexDirection: 'column',
-        // justifyContent: 'center',
-        marginBottom: 10,
-    },
+
+    // button: {
+    //     justifyContent: 'space-between',
+    //     alignItems: 'center',
+    //     flexDirection: 'column',
+    //     marginBottom: 10,
+    // },
+
 });
-
-
-export default ConnectionsLogin;
