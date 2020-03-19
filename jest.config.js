3f2a4d6c41f3119a19f43778a5acff19d075e995
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-03-19 13:27:24 -0400

    annotate jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,53 @@
+/***********************************************************
+  jest.config
+***********************************************************/
+
 module.exports = {
-    'preset': 'jest-expo',
-    'moduleFileExtensions': [ 'json', 'js', 'jsx', 'ts', 'tsx' ],
+
+    /*
+    Limit testing to...
+    */
+    'projects': [
+        /* Android */
+        {
+            'preset': 'jest-expo/android',
+        },
+        /* iOS */
+        {
+            'preset': 'jest-expo/ios',
+        },
+    ],
+    /*
+    DO test...
+    */
+    'moduleFileExtensions': [
+        /* JSON files (*.json) */
+        'json',
+        /* JavaScript files (*.{js, jsx}) */
+        'js',
+        'jsx',
+        /* TypeScript files (*.{ts, tsx}) */
+        'ts',
+        'tsx',
+    ],
+    /*
+    DO transform...
+    */
     'transform': {
-        '^.+\\.[t|j]sx?$': 'babel-jest',
+        /* JavaScript files (*.{js, jsx}) */
+        '^.+\\.js[x]?$': 'babel-jest',
+        /* TypeScript files (*.{ts, tsx}) */
+        '^.+\\.ts[x]?$': 'babel-jest',
     },
+    /*
+    DO NOT transform...
+    */
     'transformIgnorePatterns': [
+        /*
+        Files as directed by Expo.
+        source: <https://docs.expo.io/versions/latest/guides/testing-with-jest/#jest-configuration>
+        */
         'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base)',
     ],
+
 };
