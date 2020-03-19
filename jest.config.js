a217b17463feaffdd0b029dfd02f9526fb15dc76
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-03-19 13:51:47 -0400

    remove unnecessary info from comments; whitespace

--- jest.config.js
+++ jest.config.js
@@ -10,26 +10,33 @@ module.exports = {
 
     /* Limit testing to... */
     'projects': [
+
         /* Android */
         {
             'preset': 'jest-expo/android',
         },
+
         /* iOS */
         {
             'preset': 'jest-expo/ios',
         },
+
     ],
 
     /* DO test... */
     'moduleFileExtensions': [
-        /* JSON files (*.json) */
+
+        /* JSON */
         'json',
-        /* JavaScript files (*.{js, jsx}) */
+
+        /* JavaScript */
         'js',
         'jsx',
-        /* TypeScript files (*.{ts, tsx}) */
+
+        /* TypeScript */
         'ts',
         'tsx',
+
     ],
 
     /***************************************
@@ -38,19 +45,26 @@ module.exports = {
 
     /* DO transform... */
     'transform': {
-        /* JavaScript files (*.{js, jsx}) */
+
+        /* JavaScript */
         '^.+\\.js[x]?$': 'babel-jest',
-        /* TypeScript files (*.{ts, tsx}) */
+
+        /* TypeScript */
         '^.+\\.ts[x]?$': 'babel-jest',
+
     },
 
     /* DO NOT transform... */
     'transformIgnorePatterns': [
+
         /*
-        Files as directed by Expo.
+        ... as directed by Expo
         source: <https://docs.expo.io/versions/latest/guides/testing-with-jest/#jest-configuration>
         */
         'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base)',
+
     ],
+
+    /**************************************/
 
 };
