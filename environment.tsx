303c0ad1e0b40251a7c6c4846da70971724293f3
Travis Collins <TravisCollins@Koverse.com>
2020-03-20 23:47:25 -0400

    Fix for published app (needed to consider default channel)

--- environment.tsx
+++ environment.tsx
@@ -45,6 +45,9 @@ const getEnvVars = (env = Constants.manifest.releaseChannel) => {
     // This variable is set to true when react-native is running in Dev mode.
     // __DEV__ is true when run locally, but false when published.
 
+    if (env == 'default')
+        env = 'prod';
+
     return __DEV__ ? ENV.dev : ENV[env || 'prod'];
 };
 
