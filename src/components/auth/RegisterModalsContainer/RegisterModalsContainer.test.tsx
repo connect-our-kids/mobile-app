05fbf1e893f3a81ccd774279ef0c301c4634233e
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-03-23 18:27:19 -0400

    many: add basic component tests

--- src/components/auth/RegisterModalsContainer/RegisterModalsContainer.test.tsx
+++ src/components/auth/RegisterModalsContainer/RegisterModalsContainer.test.tsx
@@ -4,19 +4,19 @@ import { Platform, View } from 'react-native';
 import renderer from 'react-test-renderer';
 import { render, fireEvent } from 'react-native-testing-library';
 
-import VideoAgreeModal from './VideoAgreeModal';
+import RegisterModalsContainer from './RegisterModalsContainer';
 
 /**********************************************************/
 
-describe('VideoAgreeModal component', () => {
+describe('RegisterModalsContainer component', () => {
 
     test('renders', () => {
-        const tree = renderer.create(<VideoAgreeModal/>).toJSON();
+        const tree = renderer.create(<RegisterModalsContainer/>).toJSON();
         expect(tree.children).toHaveLength(1);
     });
 
     test('matches snapshot', () => {
-        const tree = renderer.create(<VideoAgreeModal/>).toJSON();
+        const tree = renderer.create(<RegisterModalsContainer/>).toJSON();
         expect(tree).toMatchSnapshot();
     });
 
