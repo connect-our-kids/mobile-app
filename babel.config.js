1d8a3cb36244529fc1099da4d719c874d31f7c0a
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-03-23 10:47:59 -0400

    update babel.config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
 module.exports = function (api) {
+
     api.cache(true);
+
     return {
-        presets: [ 'babel-preset-expo' ],
+
+        'presets': [
+            'babel-preset-expo',
+        ],
+
+        // 'env': {
+        //     'test': {
+        //         'plugins': [
+        //             '@babel/plugin-transform-modules-commonjs',
+        //         ],
+        //     },
+        // },
+
     };
+
 };
