780ad97e777f56b99178c6eb45daae4a02a95462
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-03-24 12:32:33 -0400

    SearchForm: move to Search/Form

--- src/components/search/Search/Form.test.jsx
+++ src/components/search/Search/Form.test.jsx
@@ -4,7 +4,7 @@ import { Platform, View } from 'react-native';
 import renderer from 'react-test-renderer';
 import { render, fireEvent } from 'react-native-testing-library';
 
-import SearchForm from './SearchForm';
+import SearchForm from './Form';
 
 /**********************************************************/
 
--- src/screens/PeopleSearchScreen.js
+++ src/screens/PeopleSearchScreen.js
@@ -27,7 +27,7 @@ import { ScrollView, FlatList } from 'react-native-gesture-handler';
 import PersonRow from '../components/Person/PersonRow';
 import headerConfig from '../helpers/headerConfig';
 import constants from '../helpers/constants';
-import SearchForm from '../components/search/SearchForm';
+import SearchForm from '../components/search/Search/Form';
 import { sendEvent } from '../helpers/createEvent';
 import Loader from '../components/Loader';
 
