0389e1647e2f48157eef417da1dc8e21f742f009
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-03-27 13:35:17 -0400

    AddEngagementForm: restructure in family-connections/

---
+++ src/components/family-connections/AddEngagementForm/AddEngagementForm.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Platform, View } from 'react-native';
+
+import renderer from 'react-test-renderer';
+import { render, fireEvent } from 'react-native-testing-library';
+
+import AddEngagementForm from './AddEngagementForm';
+
+/**********************************************************/
+
+describe('AddEngagementForm component', () => {
+
+    test('renders', () => {
+        const tree = renderer.create(<AddEngagementForm/>).toJSON();
+        expect(tree.children).toHaveLength(1);
+    });
+
+    test('matches snapshot', () => {
+        const tree = renderer.create(<AddEngagementForm/>).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+});
---
+++ src/components/family-connections/AddEngagementForm/index.js
@@ -0,0 +1,2 @@
+import AddEngagementForm from './AddEngagementForm';
+export default AddEngagementForm;
