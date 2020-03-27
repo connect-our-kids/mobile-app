6bea7f1dd75a2792c322e1ccea3a3ab0473b8a94
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-03-27 10:28:06 -0400

    fix typo

--- src/screens/SearchResultScreen.js
+++ src/screens/SearchResultScreen.js
@@ -181,7 +181,7 @@ class SearchResultScreen extends React.Component {
                           {!isLoggedIn && (
                               <TouchableHighlight onPress={this.startRegister}>
                                   <Text style={styles.link}>
-                                  his is a preview with limited results. Social workers, family recruiters, and CASA volunteers can have completely free access. Touch here to find out more.
+                                  This is a preview with limited results. Social workers, family recruiters, and CASA volunteers can have completely free access. Touch here to find out more.
                                   </Text>
                               </TouchableHighlight>
                           )}
