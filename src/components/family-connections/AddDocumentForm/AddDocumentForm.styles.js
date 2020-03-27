94d97284c96ff9d2e34d73f03bdef38ef9f4de7c
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-03-27 16:40:49 -0400

    AddDocumentForm: factor out styles

--- src/components/family-connections/AddDocumentForm/AddDocumentForm.styles.js
+++ src/components/family-connections/AddDocumentForm/AddDocumentForm.styles.js
@@ -1,9 +1,82 @@
 import { StyleSheet } from 'react-native';
 
+import constants from '../../../helpers/constants';
+
 /**********************************************************/
 
 export default StyleSheet.create({
 
-    /* styles */
+    scrollView: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: constants.backgroundColor,
+        padding: 0,
+        margin: 0,
+    },
+
+    container: {
+        justifyContent: 'flex-start',
+        alignItems: 'stretch',
+        width: '100%',
+        height: '100%',
+        padding: 8,
+        margin: 0,
+    },
+
+    content: {
+        // width: '100%',
+        margin: 8,
+    },
+
+    displayText: {
+        padding: 2,
+    },
+
+    inputText: {
+        padding: 8,
+        borderRadius: 4,
+        fontSize: 16,
+        backgroundColor: constants.inputBackgroundColor,
+    },
+
+    header: {
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+
+    attachmentInfoContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        height: '25%',
+        borderWidth: 0,
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        borderColor: constants.borderColor,
+    },
+
+    attachmentPreview: {
+        width: '40%',
+        height: '100%',
+    },
+
+    attachmentInfo: {
+        paddingLeft: 16,
+    },
+
+    saveButton: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 50,
+        borderWidth: 1,
+        borderRadius: 4,
+        borderColor: constants.highlightColor,
+        backgroundColor: constants.highlightColor,
+    },
+
+    saveButtonText: {
+        fontSize: 30,
+        color: constants.iconColor,
+    },
 
 });
--- src/components/family-connections/AddDocumentForm/AddDocumentForm.tsx
+++ src/components/family-connections/AddDocumentForm/AddDocumentForm.tsx
@@ -5,7 +5,6 @@ import {
     ScrollView,
     View,
     TouchableOpacity,
-    StyleSheet,
     TextInput,
     Image,
     KeyboardAvoidingView,
@@ -21,6 +20,7 @@ import { connect } from 'react-redux';
 import { getEngagements } from '../../../store/actions/connectionData';
 import { postConnectionDocument } from '../../../store/actions/connectionEngagements';
 
+import styles from './AddDocumentForm.styles';
 import constants from '../../../helpers/constants';
 
 /**********************************************************/
@@ -79,8 +79,8 @@ function AddDocumentForm(props: Record<string, any>): JSX.Element {
                     {(media.type === 'image') ? (
                         <Image
                             source={{ uri: attachment.uri }}
-                            style={[ styles.attachmentPreview ]}
                             resizeMode={'cover'}
+                            style={[ styles.attachmentPreview ]}
                         />
                     ) : (
                         <AttachmentIcon
@@ -103,7 +103,7 @@ function AddDocumentForm(props: Record<string, any>): JSX.Element {
                     value={title}
                     onChangeText={setTitle}
                     placeholder={'TITLE'}
-                    placeholderTextColor={'#AAA9AD'}
+                    placeholderTextColor={constants.textColor}
                     textAlignVertical={'center'}
                 />
                 {/* NOTES INPUT */}
@@ -114,7 +114,7 @@ function AddDocumentForm(props: Record<string, any>): JSX.Element {
                     multiline={true}
                     numberOfLines={4}
                     placeholder={'NOTES'}
-                    placeholderTextColor={'#AAA9AD'}
+                    placeholderTextColor={constants.textColor}
                     textAlignVertical={'top'}
                     returnKeyType={'default'}
                     enablesReturnKeyAutomatically={true}
@@ -183,82 +183,3 @@ function AddDocumentForm(props: Record<string, any>): JSX.Element {
     );
 
 }
-
-/**********************************************************/
-
-const styles = StyleSheet.create({
-
-    scrollView: {
-        width: '100%',
-        height: '100%',
-        backgroundColor: constants.backgroundColor,
-        padding: 0,
-        margin: 0,
-    },
-
-    container: {
-        justifyContent: 'flex-start',
-        alignItems: 'stretch',
-        width: '100%',
-        height: '100%',
-        padding: 8,
-        margin: 0,
-    },
-
-    content: {
-        // width: '100%',
-        margin: 8,
-    },
-
-    displayText: {
-        padding: 2,
-    },
-
-    inputText: {
-        padding: 8,
-        borderRadius: 4,
-        fontSize: 16,
-        backgroundColor: constants.inputBackgroundColor,
-    },
-
-    header: {
-        fontSize: 30,
-        fontWeight: 'bold',
-    },
-
-    attachmentInfoContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        height: '25%',
-        borderWidth: 0,
-        borderTopWidth: 1,
-        borderBottomWidth: 1,
-        borderColor: constants.borderColor,
-    },
-
-    attachmentPreview: {
-        width: '40%',
-        height: '100%',
-    },
-
-    attachmentInfo: {
-        paddingLeft: 16,
-    },
-
-    saveButton: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 50,
-        borderWidth: 1,
-        borderRadius: 4,
-        borderColor: constants.highlightColor,
-        backgroundColor: constants.highlightColor,
-    },
-
-    saveButtonText: {
-        fontSize: 30,
-        color: constants.iconColor,
-    },
-
-});
