7cec1c955a0f069cbe99b15325a6d181995361d1
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-03-27 18:04:14 -0400

    people-search/: rename from search/

--- src/screens/PeopleSearchScreen.js
+++ src/screens/PeopleSearchScreen.js
@@ -27,7 +27,7 @@ import { ScrollView, FlatList } from 'react-native-gesture-handler';
 import PersonRow from '../components/Person/PersonRow';
 import headerConfig from '../helpers/headerConfig';
 import constants from '../helpers/constants';
-import SearchForm from '../components/search/Search/Form';
+import SearchForm from '../components/people-search/people-search/Form';
 import { sendEvent } from '../helpers/createEvent';
 import Loader from '../components/Loader';
 
