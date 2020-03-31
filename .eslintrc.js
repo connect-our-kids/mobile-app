b36c63fbd2dc56cdd747126cf7a69ffe071425d8
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-03-31 14:02:07 -0400

    add import/typscript plugin -- so eslint recognizes typescript imports/exports

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
         'eslint:recommended',
         'plugin:import/errors',
         'plugin:import/warnings',
+        'plugin:import/typescript',
         /* 'plugin:jsx-a11y/recommended', */
         'plugin:react/recommended',
         /* 'plugin:react-native-a11y/recommended', */
